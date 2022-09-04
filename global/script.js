95702bf3538c959979e8b2a4a1f00ce477c22c11
dspyk77 <dspyk77@gmail.com>
2022-09-04 18:59:40 -0500

    added a function to pop up an alert window when you click Contact Me on the footer for all pages

---
+++ global/script.js
@@ -0,0 +1,4 @@
+// contact me pop up
+function contactMe() {
+  alert("Well hello! I see you want to talk!\nSend me a message on discord and let me know whats on your mind.")
+}
--- java-script-project/assets/script.js
+++ java-script-project/assets/script.js
@@ -4,3 +4,7 @@ function textToPage() {
   var post = document.querySelector("#change")
   post.innerHTML = "Hello, " + input + " welcome home! "
 }
+// contact me pop up
+function contactMe() {
+  alert("Well hello! I see you want to talk!\nSend me a message on discord and let me know whats on your mind.")
+}
