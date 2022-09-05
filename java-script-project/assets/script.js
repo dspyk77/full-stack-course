a997fa43a8fbc951a6a71523225b69ae50a45460
dspyk77 <dspyk77@gmail.com>
2022-09-05 17:33:43 -0500

    formating-isPos func

--- java-script-project/assets/script.js
+++ java-script-project/assets/script.js
@@ -4,7 +4,24 @@ function textToPage() {
   var post = document.querySelector("#change")
   post.innerHTML = "Hello, " + input + " welcome home! "
 }
-// contact me pop up
-function contactMe() {
-  alert("Well hello! I see you want to talk!\nSend me a message on discord and let me know whats on your mind.")
+// Telling user if a number is positive-negetive-NaN
+var result = document.querySelector("#isResult");
+var input = document.querySelector("#isPosOrNeg");
+
+function isPositive() {
+	val = input.value;
+
+	if (val > 0) {
+		console.log(val + " is Positive");
+		result.innerHTML = val + " is Positive";
+	} else if (val < 0) {
+		console.log(val + " is Negative");
+		result.innerHTML =val +  " is Negative";
+	} else if (val == 0) { // if (val == 0) {
+		console.log(val + " is Zero");
+		result.innerHTML = val + " is neither positive or negitive.. its just Zero";
+	} else {
+		console.log(val + " is NaN");
+    result.innerHTML = val + " is not a number... I'm not a shrink."
+	}
 }
